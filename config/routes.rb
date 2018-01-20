5c5a3e60f0b908a9c90b25ee435b973f0a780064
Madelene Campos <madelenecampos@gmail.com>
2018-01-20 15:58:20 -0500

    Creates routes within api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
 
   root to: "posts#home"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: { format: :json } do
+    resources :posts, only: [ :show ]
+  end
+
 end
