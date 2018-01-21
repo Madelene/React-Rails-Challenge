6578b600421104d60552116e6c55287513e3b03e
Madelene Campos <madelenecampos@gmail.com>
2018-01-21 17:22:47 -0500

    Renders component that contains the Router

--- app/javascript/posts/index.js
+++ app/javascript/posts/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'  
 import App from './components/App'
 
-
 const posts = document.querySelector('#posts')  
-ReactDOM.render(<App />, posts)  
+
+ReactDOM.render(<App />, posts) 
