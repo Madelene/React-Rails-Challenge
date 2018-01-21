b9f049f15745b67ab85e6963f4cad040cab79be6
Madelene Campos <madelenecampos@gmail.com>
2018-01-21 17:24:29 -0500

    Adds all actions for posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: "posts#home"
-  
+  root to: "welcomes#home"
+
   namespace :api, defaults: { format: :json } do
-    resources :posts, only: [ :show ]
+    resources :posts
   end
 
 end
